dbe56b8a4b5863ecad5dfff4f9a844bf11a06d98
Lorenzo Bandini <lboa@hotmail.it>
2024-08-12 18:49:49 +0200

    add fonts and style globally and with variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,20 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        jost: ['Jost', ...defaultTheme.fontFamily.sans]
       },
+      colors: {
+        'primary': '#FF7800',
+        'secondary' :'#CC4440',
+        'dark' : '#24222D',
+        'light': '#FAD2AD',
+        'light2': '#F7F8F7',
+      }
     },
   },
   plugins: [],
