6edc8d4aec534aedec2484ef86eb58d82ca5b1a5
Lorenzo Bandini <lboa@hotmail.it>
2024-08-19 18:16:06 +0200

    add profile icon to navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,21 +1,33 @@
 import { CiCircleInfo } from "react-icons/ci";
-import { FaUserCircle } from "react-icons/fa";
 import { Button } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function Navbar() {
   const session = await getServerAuthSession();
   const userName = session?.user?.name ?? "Ospite";
+  const userImage = session?.user?.image ?? "/profile-simple.svg"; // Percorso dell'SVG di default
 
   return (
     <nav className="flex justify-between bg-transparent p-3">
       <div className="flex items-center">
-        <Button variant={"secondary"} className="py-2">
-          <FaUserCircle size={38} className="mr-2" />
+        <Button variant={"secondary"} className="py-2 flex items-center">
+          <div className="relative h-12 mr-2 flex items-center justify-center">
+            <Image
+              src={userImage}
+              alt="User profile"
+              width={34}  
+              height={34} 
+              className="rounded-full"
+            />
+          </div>
           <span>{userName}</span>
         </Button>
-        <Link href={session ? "/api/auth/signout" : "/api/auth/signin"} className="ml-4">
+        <Link
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
+          className="ml-4"
+        >
           <Button variant={session ? "destructive" : "secondary"}>
             {session ? "Sign out" : "Sign in"}
           </Button>
