fc054eda92faaeae9d43a38df42bb5415d2bea4d
Lorenzo Bandini <lboa@hotmail.it>
2024-08-19 02:45:51 +0200

    remove useless stuffs and insert my stuffs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
+import { Jost as FontSans } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { cn } from "~/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata = {
   title: "SpeedyGuesser",
-  description: "Game to be played by three people where two people have to make the third person guess as many words as possible by saying one word each",
+  description:
+    "Game to be played by three people where two people have to make the third person guess as many words as possible by saying one word each",
   icons: [{ rel: "icon", url: "/speedyguessericon.ico" }],
 };
 
@@ -16,8 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
+    <html lang="en" className={`${fontSans.variable}`}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
