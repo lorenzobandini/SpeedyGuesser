ce02c4b1267fdf46d7d5a092e96a767479f92fcc
Lorenzo Bandini <lboa@hotmail.it>
2024-08-20 17:30:59 +0200

    adding my personal styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-      },
+        personal: "px-6 py-3 text-2xl font-bold text-white bg-[#1a1a2e] rounded-full shadow-inner",
+        personalIcon: "text-white  bg-[#1a1a2e] rounded-full shadow-inner",},
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
