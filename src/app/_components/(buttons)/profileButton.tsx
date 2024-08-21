fb36de7d255a12d0abfd8a0d05e4aa26149c1460
Lorenzo Bandini <lboa@hotmail.it>
2024-08-21 02:10:10 +0200

    slit guest and profile button class

--- src/app/_components/(buttons)/profileButton.tsx
+++ src/app/_components/(buttons)/profileButton.tsx
@@ -1,43 +1,57 @@
 import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 import { getServerAuthSession } from "~/server/auth";
 import Image from "next/image";
 
 export default async function ProfileButton() {
   const session = await getServerAuthSession();
-  const userName = session?.user?.name ?? "Ospite";
-  const userImage = session?.user?.image ?? "/profile-simple.svg";
+  const userName = session?.user?.name 
+  const userImage = session?.user?.image 
 
   return (
-    <Button variant={"personal"} className="group flex items-center py-2">
-      <div className="relative mr-2 flex h-12 items-center justify-center">
-        {session?.user?.image ? (
-          <Image
-            src={userImage}
-            alt="User profile"
-            width={34}
-            height={34}
-            className="rounded-full"
-          />
-        ) : (
-          <>
-            <Image
-              src="/profile-simple.svg"
-              alt="User profile"
-              width={34}
-              height={34}
-              className="rounded-full flex"
-            />
-            <Image
-              src="/profile-simple-hover.svg"
-              alt="User profile hover"
-              width={34}
-              height={34}
-              className="rounded-full hidden group-hover:flex absolute"
-            />
-          </>
-        )}
-      </div>
-      <span>{userName}</span>
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"personal"} className="group flex items-center py-2">
+          <div className="relative mr-2 flex h-12 items-center justify-center">
+            {userImage &&
+              <Image
+                src={userImage}
+                alt="User profile"
+                width={34}
+                height={34}
+                className="rounded-full"
+              />         
+            }
+          </div>
+          <span>{userName}</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Profile Stats</DialogTitle>
+          <DialogDescription>
+            Anyone who has this link will be able to view this. {userName}
+            Anyone who has this link will be able to view this. {userName}
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">ciaoo</div>
+        <DialogFooter className="sm:justify-end">
+          <DialogClose asChild>
+            <Button variant={"personalDestructive"} size={"sm"}>
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,13 +1,16 @@
 import InfoButton  from "./(buttons)/infoButton";
 import LogButton from "./(buttons)/logButton";
 import ProfileButton from "./(buttons)/profileButton";
+import GuestProfileButton from "./(buttons)/guestProfileButton";
+import { getServerAuthSession } from "~/server/auth";
 
 export default async function Navbar() {
+  const session = await getServerAuthSession();
 
   return (
     <nav className="flex justify-between bg-transparent p-3">
       <div className="flex items-center">
-        <ProfileButton />
+        {session ? <ProfileButton /> : <GuestProfileButton />}
         <LogButton />
       </div>
       <InfoButton />
