7681ddf862956df9420d1c2708bcff573b6b83a9
Lorenzo Bandini <lboa@hotmail.it>
2024-11-06 14:17:43 +0100

    adapt schema with new functionalities and create a ts file for export types and stay consistent in the db

---
+++ src/types/enums.ts
@@ -0,0 +1,23 @@
+export enum GameType {
+  SINGLE_DEVICE = "SINGLE_DEVICE",
+  LOCAL_MULTIPLAYER = "LOCAL_MULTIPLAYER",
+  ONLINE_MULTIPLAYER = "ONLINE_MULTIPLAYER",
+}
+
+export enum WordStatus {
+  PENDING = "PENDING",
+  CORRECT = "CORRECT",
+  WRONG = "WRONG",
+  SKIPPED = "SKIPPED",
+}
+
+export enum PlayerRole {
+  GUESSER = "GUESSER",
+  HINT_GIVER = "HINT_GIVER",
+}
+
+export enum GameStatus {
+  WAITING = "WAITING",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
+}
